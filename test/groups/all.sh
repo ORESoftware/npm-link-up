ef03e4790cefcf46217de29b7cacf9de67aa0c95
Olegzandr? <alex@oresoftware.com>
2017-04-16 22:36:18 -0700

    pdev:set

--- test/groups/all.sh
+++ test/groups/all.sh
@@ -4,4 +4,5 @@
 git clone https://github.com/ORESoftware/npm-link-up.git &&
 cd npm-link-up &&
 npm install &&
+sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}
 node test/src/all.test.js
