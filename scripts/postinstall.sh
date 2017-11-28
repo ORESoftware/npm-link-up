34692ebc435181ad8f50a144d12e373ff7210f9b
Olegzandr? <alex@oresoftware.com>
2017-11-27 22:18:39 -0800

    pdev:set

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,17 +2,29 @@
 
 set -e;
 
+echo "npm-link-up postinstall routine has just started."
+
 mkdir -p "$HOME/.npmlinkup/global"
-cd "$HOME/.npmlinkup/global"
 
-if [[ ! -f package.json ]]; then
-    npm init -f
-fi
 
-if [[ "$oresoftware_env" == "alex" ]]; then
-    npm link npm-link-up -f
-else
-    npm install -S npm-link-up
-fi
+(
+    # run this stuff in a subshell
+    cd "$HOME/.npmlinkup/global"
 
+    echo "npm-link-up pwd: $(pwd)"
+
+    if [[ ! -f package.json ]]; then
+        npm init -f
+    fi
+
+    if [[ "$oresoftware_env" == "alex" ]]; then
+        npm link npm-link-up -f
+    else
+        npm install -S npm-link-up@latest
+    fi
+
+)
+
+
+echo "npm-link-up postinstall routine has completed successfully."
 
