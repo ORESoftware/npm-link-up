6809b3aec132211325ff1ecf97ac3ca5e5c273e5
Olegzandr? <alex@oresoftware.com>
2018-03-12 21:22:43 -0700

    pdev:set

--- lib/cmd-line-opts.d.ts
+++ lib/cmd-line-opts.d.ts
@@ -2,25 +2,31 @@ declare const _default: ({
     names: string[];
     type: string;
     help: string;
+    default?: undefined;
+    hidden?: undefined;
 } | {
     names: string[];
     type: string;
     help: string;
     default: number;
+    hidden?: undefined;
 } | {
     names: string[];
     type: string;
     help: string;
     default: boolean;
+    hidden?: undefined;
 } | {
     names: string[];
     type: string;
     help: string;
     hidden: boolean;
+    default?: undefined;
 } | {
     names: string[];
     type: string;
     help: string;
     default: string;
+    hidden?: undefined;
 })[];
 export default _default;
