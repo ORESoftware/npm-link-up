e9fe81759915a671c7667f5e5739a937532ca305
Olegzandr Denman <alex@oresoftware.com>
2018-05-31 18:33:28 -0700

    pdev:set

---
+++ assets/nlu.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+
+
+nlu(){
+
+  local which_nlu="$(command -v nlu)"
+
+  if [ -z "$which_nlu" ]; then
+        npm install -g --loglevel=warn "npm-link-up"  || {
+          return 1;
+      }
+  fi
+
+  command nlu "$@"
+}
+
+
+npmlinkup(){
+
+  local which_nlu="$(command -v npmlinkup)"
+
+  if [ -z "$which_nlu" ]; then
+        npm install -g --loglevel=warn "npm-link-up"  || {
+          return 1;
+      }
+  fi
+
+  command nlu "$@"
+}
--- cli/index.sh
+++ cli/index.sh
@@ -6,14 +6,8 @@ RL="$(readlink "$0")";
 EXECDIR="$(dirname $(dirname "$RL"))";
 MYPATH="$DIRN/$EXECDIR";
 project_root="$(cd $(dirname ${MYPATH}) && pwd)/$(basename ${MYPATH})";
-
-
-npm_root="${project_root}/node_modules";
-more_path="${npm_root}/.bin";
-export PATH="${more_path}:${PATH}";
-
-echo " => NPM version used by npmlinkup => $(npm -v)";
-
+npm_root_bin="${project_root}/node_modules/.bin";
+export PATH="${npm_root_bin}:${PATH}";
 node "${project_root}/dist/index.js" $@;
 
 
