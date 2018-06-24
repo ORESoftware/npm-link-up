ff7d306f4f904bed68f08df223a2818d57a13f4c
Olegzandr Denman <alex@oresoftware.com>
2018-06-23 23:21:33 -0700

    pdev:set

--- assets/nlu.sh
+++ assets/nlu.sh
@@ -1,34 +1,42 @@
 #!/usr/bin/env bash
 
-
+get_latest_source_nlu(){
+ . "$HOME/.oresoftware/bash/nlu.sh"
+}
 
 nlu(){
 
-  local which_nlu="$(command -v nlu)"
+  if ! type -f nlu &> /dev/null || ! which nlu &> /dev/null; then
 
-  if [ -z "$which_nlu" ]; then
-        npm install -g --loglevel=warn "npm-link-up"  || {
+        echo "Installing the npm-link-up package globaly..."
+        npm i -s -g "npm-link-up"  || {
+          echo -e "Could not install 'npm-link-up' globally.";
+          echo -e "Please check your permissions to install NPM packages globally.";
           return 1;
       }
   fi
 
-  command nlu "$@"
+  command nlu $@
 }
 
 
 npmlinkup(){
 
-  local which_nlu="$(command -v npmlinkup)"
+  if ! type -f npmlinkup &> /dev/null || ! which npmlinkup &> /dev/null; then
 
-  if [ -z "$which_nlu" ]; then
-        npm install -g --loglevel=warn "npm-link-up"  || {
+        echo "Installing the npm-link-up package globaly..."
+        npm i -s -g "npm-link-up"  || {
+
+          echo -e "Could not install 'npm-link-up' globally.";
+          echo -e "Please check your permissions to install NPM packages globally.";
           return 1;
       }
   fi
 
-  command npmlinkup "$@"
+  command npmlinkup $@
 }
 
 
 export -f nlu;
 export -f npmlinkup;
+export -f get_latest_source_nlu;
