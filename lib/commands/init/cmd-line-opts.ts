dbca469ba80f50384e2bbef02b0d44a884115aa9
Olegzandr Denman <alex@oresoftware.com>
2018-07-12 16:19:38 -0700

    pdev:set

--- lib/commands/init/cmd-line-opts.ts
+++ lib/commands/init/cmd-line-opts.ts
@@ -13,7 +13,7 @@ export default  [
     names: ['verbosity', 'v'],
     type: 'positiveInteger',
     help: 'Verbosity level is an integer between 1 and 4, inclusive.',
-    default: 3
+    default: 2
   },
 
   {
